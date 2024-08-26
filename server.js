685685f2d774458d0d138fc50e562a14f909dac6
richarq-fisi <temporal98x@gmail.com>
2024-08-26 12:51:29 -0500

    socket chat

---
+++ server.js
@@ -0,0 +1,36 @@
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
+
+dotenv.config();
+
+const __dirname = path.resolve();
+const PORT = process.env.PORT || 5000;
+
+// MIDDLEWARES
+app.disable('x-powered-by');
+app.use(express.json());
+app.use(cookieParser());
+
+// ROUTES
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+//app.use(express.static(path.join(__dirname, "/views")));
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+server.listen(PORT, () => {
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`);
+});
